4cddd0bceef940ef00e2ea9a7dc5cf2f76e3e75a
Kel <kellylimyaujade@gmail.com>
2024-07-26 23:51:40 +0800

    UPDATE: User Profile and Change Password and Message Box

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -8,7 +8,9 @@ urlpatterns = [
     path('project/', views.project, name='project'),
     path('community/', views.community, name='community'),
     path('profile/', views.profile, name='profile'),
+    path('change_password/', views.change_password, name='change_password'),
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
     path('signout/', views.signout, name='signout'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 
 # Create your views here.
 def home(request):
@@ -18,19 +21,19 @@ def signup(request):
         conpassword = request.POST['conpassword']
 
         if User.objects.filter(username=username).exists():
-            messages.error(request, 'Username already exists')
+            messages.error(request, 'Username already exists', extra_tags='warning')
         elif User.objects.filter(email=email).exists():
-            messages.error(request, 'Email already exists')
+            messages.error(request, 'Email already exists', extra_tags='warning')
         elif password != conpassword:
-            messages.error(request, 'Passwords do not match')
+            messages.error(request, 'Passwords do not match', extra_tags='warning')
         elif not username.isalnum():
-            messages.error(request, 'Username should only contain letters and numbers')
+            messages.error(request, 'Username should only contain letters and numbers', extra_tags='warning')
         else:
             myuser = User.objects.create_user(username, email, password)
             myuser.first_name = fname
             myuser.last_name = lname
             myuser.save()
-            messages.success(request, 'Your account has been created successfully')
+            messages.success(request, 'Your account has been created successfully', extra_tags='success')
             return redirect('home') 
 
         return redirect('signup')
@@ -51,15 +54,41 @@ def signin(request):
             return render(request, 'authentication/index.html',{'fname':fname})
 
         else:
-            messages.error(request, 'Invalid credentials')
+            messages.error(request, 'Invalid credentials', extra_tags='warning')
             return redirect('home')
         
     return render(request, 'authentication/signin.html')
 
 def signout(request):
     logout(request)
-    messages.success(request, 'You have been logged out successfully')
+    messages.success(request, 'You have been logged out successfully', extra_tags='success')
     return redirect('home')
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        user = request.user
+        user.first_name = request.POST['fname']
+        user.last_name = request.POST['lname']
+        user.save()
+        messages.success(request, 'Your profile has been updated successfully')
+        return redirect('profile')
+    return render(request, 'authentication/profile.html')
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!', extra_tags='success')
+            return redirect('profile')
+        else:
+            messages.error(request, 'Please correct the error below.', extra_tags='warning')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'authentication/change_password.html', {'form': form})
 
 def subscription(request):
     return render(request, 'subscription.html')
@@ -70,5 +99,3 @@ def project(request):
 def community(request):
     return render(request, 'community.html')
 
-def profile(request):
-    return render(request, 'profile.html')
